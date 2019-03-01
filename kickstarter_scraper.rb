aaf69df6083b7f3c5c88a84d9c2b11942d3956bd
Nissan Holzer <nissan.holzer@gmail.com>
2019-03-01 18:53:49 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,10 +15,11 @@ def create_project_hash
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("span.location-name").text,
-      
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
   
+  #return the projects hash
   projects
 end
 
